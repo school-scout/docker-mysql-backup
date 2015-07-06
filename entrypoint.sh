85f3749be0fc85d9dd290ae10bbf732a2d0e4c7a
Martin Honermeyer <martin.honermeyer@zweitag.de>
2015-07-06 19:44:26 +0200

    Add some progress messages

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ BACKUP_FILE=${BACKUP_DIR}/$(date --utc +%Y-%m-%d).sql.gpg
 mkdir ${BACKUP_DIR}
 
 # Do a full dump of the database, flushing the binlogs, encrypting the result
+echo Dumping data
 mysqldump -h mysql -u ${MYSQL_BACKUP_USER} -p${MYSQL_BACKUP_PASSWORD} --single-transaction --flush-logs --master-data=2 --all-databases \
   | gpg2 -c --batch --passphrase ${MYSQL_BACKUP_ENCRYPTION_PASSPHRASE} >$BACKUP_FILE
 
@@ -15,6 +16,7 @@ echo $MYSQL_BACKUP_SSH_KEY | base64 -d >/root/.ssh/id_rsa
 chmod 0600 /root/.ssh/id_rsa
 
 # Copy dump to remote server
+echo Copying files to backup server: $(ls $BACKUP_DIR)
 scp -r -P ${MYSQL_BACKUP_SSH_PORT:=22} -oStrictHostKeyChecking=no $BACKUP_DIR $MYSQL_BACKUP_SSH_ADDRESS
 
 echo "Backup done."
